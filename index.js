c2051aaba6c8686f8dbae76e34b22266b0baf4c0
Yan <Exm230600@gmail.com>
2023-07-06 00:03:48 +0200

    Full homework 3

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-const choiceInputs = document.querySelectorAll('.difficulty__box_input')
-const choiceButton = document.querySelector(".difficulty__box_button")
+const choiceInputs = document.querySelectorAll(".difficulty__box_input");
+const choiceButton = document.querySelector(".difficulty__box_button");
 
 import { randerGameBoard } from "./render.js";
 
-export let difficulty = ''
+export let difficulty = "";
 
-for (let choiceInput of choiceInputs ) {
-    choiceInput.addEventListener('click', function (e) {
-        difficulty = e.target.value,
-        buttonDisabled(difficulty)
-        
+for (let choiceInput of choiceInputs) {
+    choiceInput.addEventListener("click", function (e) {
+        (difficulty = e.target.value), buttonDisabled(difficulty);
     });
-    
 }
 
 function buttonDisabled(difficulty) {
     if (difficulty) {
-        choiceButton.removeAttribute('disabled')
+        choiceButton.removeAttribute("disabled");
     }
 }
 
-choiceButton.addEventListener('click', randerGameBoard)
-
+choiceButton.addEventListener("click", randerGameBoard);
